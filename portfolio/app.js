ad83ea1f68196f93fd4d9a243f223e117a6a105a
nasim313github <nasimsarder2003@gmail.com>
2024-05-20 12:55:13 +0200

    adding email api

--- portfolio/app.js
+++ portfolio/app.js
@@ -5,3 +5,28 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.right='-200px';
 }
+// Get the modal
+var modal = document.getElementById("formModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("send_email_btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the div, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
